cccf94a8994e2e020540e01f752fb4eef4d6851a
Shun <47809066+saitoshi@users.noreply.github.com>
2021-08-20 17:20:58 +0900

    fix who

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -6,15 +6,24 @@ import { Header, Container, Segment } from 'semantic-ui-react';
 class　Who extends React.Component {
   render() {
     return (
-        <div className='who'>
-          <br/>
-          <br/>
-          <Segment>
-            <Segment inverted color = 'teal'><Header as = 'h2'>自己紹介</Header>
+        <div>
+          <div className='who'>
+            <br/>
+            <br/>
+            <Segment>
+              <Segment inverted color = 'teal'><Header as = 'h2'>自己紹介</Header>
+              </Segment>
+              <p>関西学院大学卒業後、銀行に就職　筋トレを始め、その魅力に取り導かれる。
+                NSCA-PTの資格を取得し、パーソナルトレーナーとしての活動を開始した。　SNSの総フォロワー数は合計で３万人を突破した。</p>
             </Segment>
-            <p>関西学院大学卒業後、銀行に就職　筋トレを始め、その魅力に取り導かれる。
-              NSCA-PTの資格を取得し、パーソナルトレーナーとしての活動を開始した。　SNSの総フォロワー数は合計で３万人を突破した。</p>
+          </div>
+          <br/>
+          <Container>
+            <Segment>
+            <Segment inverted color = 'teal'><Header as = 'h2'>筋トレ履歴</Header></Segment>
+            <p></p>
           </Segment>
+          </Container>
         </div>
     );
   }
