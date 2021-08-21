8dd00c44bc19d77ca4822e168805cded88286d89
Shun <47809066+saitoshi@users.noreply.github.com>
2021-08-22 08:22:55 +0900

    fix who page

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Header, Container, Segment } from 'semantic-ui-react';
+import { Header, Image, Segment } from 'semantic-ui-react';
 import SideNavBar from './SideNavBar';
 
 class　Who extends React.Component {
@@ -17,6 +17,10 @@ class　Who extends React.Component {
             </Segment>
             <p>関西学院大学卒業後、銀行に就職　筋トレを始め、その魅力に取り導かれる。
               NSCA-PTの資格を取得し、パーソナルトレーナーとしての活動を開始した。　SNSの総フォロワー数は合計で３万人を突破した。</p>
+              <Image centered size = 'large' src = "https://pbs.twimg.com/media/E9Tnq52VIAMbdJR?format=jpg&name=large"/>
+          </Segment>
+          <Segment>
+            <Segment inverted color = 'teal'><Header as = 'h2' textAlign = 'center'>Ambassador</Header></Segment>
           </Segment>
         </div>
         </div>
